3ca8e1323173bf6eba50f6a684b91efa27eae289
Nathan Schneider <nschneid@cs.cmu.edu>
2012-05-16 00:00:12 -0400

    README and license

--- src/edu/cmu/ark/LabeledSentence.java
+++ src/edu/cmu/ark/LabeledSentence.java
@@ -1,3 +1,20 @@
+/*  
+    AQMAR Arabic Tagger: Sequence tagger with cost-augmented structured perceptron training
+    Copyright (C) 2012  Behrang Mohit, Nathan Schneider, Rishav Bhowmick, Kemal Oflazer, and Noah A. Smith
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package edu.cmu.ark;
 
 import java.util.*;
