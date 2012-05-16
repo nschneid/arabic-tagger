745c3a5003428672865f946514f2b885bbcecabe
Nathan Schneider <nschneid@cs.cmu.edu>
2012-05-16 19:37:52 -0400

    Make arabic-tagger.jar executable

--- build.sh
+++ build.sh
@@ -9,7 +9,10 @@ mkdir -p bin
 javac -cp .:lib/JSAP-2.1.jar -d bin src/edu/cmu/ark/util/LineChunkReader.java src/edu/cmu/ark/DiscriminativeTagger.java src/edu/cmu/ark/LabeledSentence.java src/edu/cmu/ark/ArabicFeatureExtractor.java
 
 cd bin
-jar cf $TARGET *
+echo "Main-Class: edu.cmu.ark.DiscriminativeTagger
+Class-Path: lib/JSAP-2.1.jar
+" > manifest.txt
+jar cmf manifest.txt $TARGET *
 cd ..
 
 mv bin/$TARGET .
